90baa1ec1d102e06d2d5e60049e954ab915bcf64
Richard George <rcgeorge23@gmail.com>
2017-02-11 11:41:19 +0000

    added training labels csv, started creating image reader

---
+++ src/main/java/com/novinet/catdog/ImageReader.java
@@ -0,0 +1,8 @@
+package com.novinet.catdog;
+
+public class ImageReader {
+	private static final String TRAINING_SET_PATH = "/Users/rcgeorge23/Downloads/train";
+	private static final String TRAINING_SET_LABELS_FILENAME = "trainLabels.csv";
+	
+	
+}
