4b0fb4377650d304541563fc4aa1bb9209e76d2e
Richard George <rcgeorge23@gmail.com>
2017-02-13 14:13:03 +0000

    added encog dependency to build.gradle. also bit of a tidy up of the image loader class

--- src/main/java/com/novinet/catdog/TrainingsetLoader.java
+++ src/main/java/com/novinet/catdog/TrainingsetLoader.java
@@ -3,53 +3,55 @@ package com.novinet.catdog;
 import static java.lang.String.format;
 import static java.util.Arrays.asList;
 import static javax.imageio.ImageIO.read;
+import static org.apache.commons.csv.CSVFormat.EXCEL;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
 import org.apache.log4j.Logger;
 
 import com.novinet.catdog.util.FastRgbBufferedImageWrapper;
 
-public class ImageReader {
+public class TrainingsetLoader {
 
 	private static final int COLUMN_INDEX_IMAGE_NUMBER = 0;
 	private static final int COLUMN_INDEX_IMAGE_LABEL = 1;
 	
-	private static final String LABEL_DOG = "dog";
-	private static final String LABEL_CAT = "cat";
-	
-	private static final String TRAINING_SET_PATH = "/Users/rcgeorge23/Downloads/train/";
-	private static final String TRAINING_SET_LABELS_FILENAME = "/trainLabels.csv";
-	
 	private List<AnnotatedImage> annotatedImages = new ArrayList<AnnotatedImage>();
 	
-	private final static Logger LOGGER = Logger.getLogger(ImageReader.class);
+	private final static Logger LOGGER = Logger.getLogger(TrainingsetLoader.class);
+	
+	private List<String> labels;
+	private String trainingsetPath;
+	private Iterable<CSVRecord> trainingsetRecords;
+	private String trainingImageFileExtension;
 
-	public ImageReader(List<String> labels) throws IOException {
-		Reader in = new InputStreamReader(this.getClass().getResourceAsStream(TRAINING_SET_LABELS_FILENAME));
-		Iterable<CSVRecord> records = CSVFormat.EXCEL.parse(in);
-		
+	public TrainingsetLoader(List<String> labels, String trainingsetPath, String trainingImageFileExtension, Iterable<CSVRecord> trainingsetRecords) throws IOException {
+		this.labels = labels;
+		this.trainingsetPath = trainingsetPath;
+		this.trainingsetRecords = trainingsetRecords;
+		this.trainingImageFileExtension = trainingImageFileExtension;
+	}
+	
+	public void initialise() throws IOException {
 		boolean currentRowIsHeader = true;
 		
-		for (CSVRecord record : records) {
+		for (CSVRecord record : trainingsetRecords) {
 			if (currentRowIsHeader) {
 				currentRowIsHeader = false;	
 				//skip the header row
 				continue;
 			}
 			
-			String filename = record.get(COLUMN_INDEX_IMAGE_NUMBER) + ".png";
+			String filename = record.get(COLUMN_INDEX_IMAGE_NUMBER) + trainingImageFileExtension;
 			String label = record.get(COLUMN_INDEX_IMAGE_LABEL);
 			
 			if (labels.contains(label)) {
-				annotatedImages.add(buildAnnotatedImage(TRAINING_SET_PATH + filename, label));
+				annotatedImages.add(buildAnnotatedImage(trainingsetPath + filename, label));
 			}
 		}
 		
@@ -69,6 +71,11 @@ public class ImageReader {
 	}
 
 	public static void main(String[] args) throws IOException {
-		new ImageReader(asList(LABEL_CAT, LABEL_DOG));
+		TrainingsetLoader trainingsetLoader = new TrainingsetLoader(asList("cat", "dog"), "/Users/rcgeorge23/Downloads/train/", ".png", EXCEL.parse(new InputStreamReader(TrainingsetLoader.class.getResourceAsStream("/trainLabels.csv"))));
+		trainingsetLoader.initialise();
+	}
+
+	public List<AnnotatedImage> getAnnotatedImages() {
+		return annotatedImages;
 	}
 }
