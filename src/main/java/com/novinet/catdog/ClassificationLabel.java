36a93bf1386f669318defbbeec8efe1987882593
Richard George <rcgeorge23@gmail.com>
2018-03-18 20:15:45 +0000

    bit of tidy up

--- src/main/java/com/novinet/catdog/ClassificationLabel.java
+++ src/main/java/com/novinet/catdog/ClassificationLabel.java
@@ -1,8 +1,8 @@
 package com.novinet.catdog;
 
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.apache.commons.lang3.builder.HashCodeBuilder;
-import org.apache.commons.lang3.builder.ToStringBuilder;
+import static org.apache.commons.lang3.builder.EqualsBuilder.reflectionEquals;
+import static org.apache.commons.lang3.builder.HashCodeBuilder.reflectionHashCode;
+import static org.apache.commons.lang3.builder.ToStringBuilder.reflectionToString;
 
 public class ClassificationLabel {
     private String label;
@@ -23,16 +23,16 @@ public class ClassificationLabel {
 
     @Override
     public boolean equals(Object obj) {
-        return EqualsBuilder.reflectionEquals(this, obj);
+        return reflectionEquals(this, obj);
     }
 
     @Override
     public int hashCode() {
-        return HashCodeBuilder.reflectionHashCode(this);
+        return reflectionHashCode(this);
     }
 
     @Override
     public String toString() {
-        return ToStringBuilder.reflectionToString(this);
+        return reflectionToString(this);
     }
 }
--- src/main/java/com/novinet/catdog/ClassificationService.java
+++ src/main/java/com/novinet/catdog/ClassificationService.java
@@ -10,6 +10,8 @@ import java.util.Map;
 
 import static com.novinet.catdog.TrainingSetLoader.CAT;
 import static com.novinet.catdog.TrainingSetLoader.DOG;
+import static java.lang.Math.abs;
+import static java.lang.Math.round;
 import static javax.imageio.ImageIO.read;
 
 public class ClassificationService {
@@ -19,8 +21,8 @@ public class ClassificationService {
     static final Map<Long, ClassificationLabel> CLASSIFICATIONS = new HashMap<>();
 
     static {
-        CLASSIFICATIONS.put(Math.round(CAT.getNetworkOutput()), CAT);
-        CLASSIFICATIONS.put(Math.round(DOG.getNetworkOutput()), DOG);
+        CLASSIFICATIONS.put(round(CAT.getNetworkOutput()), CAT);
+        CLASSIFICATIONS.put(round(DOG.getNetworkOutput()), DOG);
     }
 
     public ClassificationResult classify(InputStream imageInputStream) throws IOException {
@@ -30,10 +32,8 @@ public class ClassificationService {
     public ClassificationResult classify(InputStream imageInputStream, InputStream networkStream) throws IOException {
         NaiveNeuralNetwork naiveNeuralNetwork = new NaiveNeuralNetwork(networkStream);
         double classification = naiveNeuralNetwork.classify(buildBiw(imageInputStream));
-        Long result = Math.round(classification);
-        ClassificationLabel classificationLabel = CLASSIFICATIONS.get(result);
-        double certainty = Math.abs(1 - classification);
-        return new ClassificationResult(certainty, classificationLabel);
+        ClassificationLabel classificationLabel = CLASSIFICATIONS.get(round(classification));
+        return new ClassificationResult(abs(1 - classification), classificationLabel);
     }
 
     private FastRgbBufferedImageWrapper buildBiw(InputStream imageInputStream) throws IOException {
